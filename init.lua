a704d11b49910b6fdbddc02856f786f783e5ad1f
jessejp <jessepernila@gmail.com>
2025-10-14 18:07:29 +0300

    added terminal binds and Oil

--- init.lua
+++ init.lua
@@ -3,8 +3,9 @@ vim.g.mapleader = "<Space>"
 require("config.lazy")
 require("config.lsp")
 
+vim.o.tabstop = 4
+vim.opt.shiftwidth = 2
 vim.opt.winborder = "rounded"
-vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
@@ -32,3 +33,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+  end
+})
+
+--local job_id = 0
+vim.keymap.set("n", "<leader>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 8)
+
+  --job_id = vim.bo.channel
+end)
+
+-- Open Oil file viewer in current buffer's directory
+vim.keymap.set("n", "-", "<cmd>Oil<CR>")
