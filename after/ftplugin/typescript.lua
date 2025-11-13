896422066c889a36f66d815f60acb3974c53e3ca
jessejp <jessepernila@gmail.com>
2025-11-13 22:14:42 +0200

    rebind debugger + add keymap for test current file with deno

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,3 +1,9 @@
 local set = vim.opt_local
 
 set.shiftwidth = 2
+
+vim.keymap.set("n", "<leader>tf", "<cmd>terminal deno test %<CR>", {
+  buffer = true,
+  silent = true,
+  desc = "Test file with deno",
+})
--- lua/config/plugins/debugger.lua
+++ lua/config/plugins/debugger.lua
@@ -49,9 +49,9 @@ return {
       end)
 
       vim.keymap.set("n", "<F8>", dap.continue)
-      vim.keymap.set("n", "<F10>", dap.step_over)
-      vim.keymap.set("n", "<F11>", dap.step_into)
-      vim.keymap.set("n", "<S-F11>", dap.step_out)
+      vim.keymap.set("n", "<F9>", dap.step_over)
+      vim.keymap.set("n", "<F10>", dap.step_into)
+      vim.keymap.set("n", "<S-F10>", dap.step_out)
       -- vim.keymap.set("n", "<F5>", dap.step_back)
       vim.keymap.set("n", "<F12>", dap.restart)
 
