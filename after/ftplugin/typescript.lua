edd4a91af8aca9df640f82371e8beb9a20276e9b
jessejp <jessepernila@gmail.com>
2025-11-15 13:19:04 +0200

    fix ts deno test not allow read from files

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -2,7 +2,7 @@ local set = vim.opt_local
 
 set.shiftwidth = 2
 
-vim.keymap.set("n", "<leader>tf", "<cmd>terminal deno test %<CR>", {
+vim.keymap.set("n", "<leader>tf", "<cmd>terminal deno test % --allow-read<CR>", {
   buffer = true,
   silent = true,
   desc = "Test file with deno",
